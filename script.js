66aa0d9c04f081bd0c8b1f08380de8121bc628eb
sbfcorgs <sbfcorgs@gmail.com>
2026-02-12 22:36:07 +0400

    Implement dynamic image grid in script.js
    Added dynamic image grid functionality to the script.

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ setInterval(()=>{
   welcome.style.opacity = opacity;
 }, 30);
 console.log("SBFC Organization Website Loaded Successfully");
+// Dynamic image grid
+const homeGallery = document.getElementById("home-gallery");
+for (let i = 1; i <= 9; i++) {
+  const div = document.createElement("div");
+  div.className = "grid-item";
+  const img = document.createElement("img");
+  img.src = `https://picsum.photos/400/300?random=${i}`;
+  div.appendChild(img);
+  homeGallery.appendChild(div);
+}
+
 
