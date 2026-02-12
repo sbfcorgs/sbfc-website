372766505bd4271e9cbad53b162585f035d86544
sbfcorgs <sbfcorgs@gmail.com>
2026-02-12 19:38:58 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example: Animate Welcome Text
+const welcome = document.querySelector('.welcome-text');
+let opacity = 0;
+let increasing = true;
+setInterval(()=>{
+  if(opacity >= 1) increasing=false;
+  if(opacity <= 0.3) increasing=true;
+  opacity += increasing ? 0.01 : -0.01;
+  welcome.style.opacity = opacity;
+}, 30);
