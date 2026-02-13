41f6027361914bc196e62546d110673c11b9842a
sbfcorgs <sbfcorgs@gmail.com>
2026-02-14 00:24:58 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-let currentIndex = 0;
-
-const images = [
-  "images/img1.jpg",
-  "images/img2.jpg",
-  "images/img3.jpg",
-  "images/img4.jpg",
-  "images/img5.jpg",
-  "images/img6.jpg"
-];
-
-function openModal(index){
-  currentIndex = index;
-  document.getElementById("imageModal").style.display = "block";
-  document.getElementById("modalImg").src = images[currentIndex];
-}
-
-function closeModal(){
-  document.getElementById("imageModal").style.display = "none";
-}
-
-function changeSlide(direction){
-  currentIndex += direction;
-
-  if(currentIndex < 0){
-    currentIndex = images.length - 1;
-  }
-
-  if(currentIndex >= images.length){
-    currentIndex = 0;
-  }
-
-  document.getElementById("modalImg").src = images[currentIndex];
-}
+// Optional JS for smooth scrolling
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if(this.hash !== "") {
+      e.preventDefault();
+      const target = document.querySelector(this.hash);
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
