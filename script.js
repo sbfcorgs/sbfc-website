2231c33959768e4142490bc1f9ad40349b4ad31d
sbfcorgs <sbfcorgs@gmail.com>
2026-02-13 18:35:05 +0400

    Refactor image modal functions and remove animations
    Removed welcome text animation and dynamic image grid. Added functions to open and close image modal.

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// Example: Animate Welcome Text
-const welcome = document.querySelector('.welcome-text');
-let opacity = 0;
-let increasing = true;
-setInterval(()=>{
-  if(opacity >= 1) increasing=false;
-  if(opacity <= 0.3) increasing=true;
-  opacity += increasing ? 0.01 : -0.01;
-  welcome.style.opacity = opacity;
-}, 30);
-console.log("SBFC Organization Website Loaded Successfully");
-// Dynamic image grid
-const homeGallery = document.getElementById("home-gallery");
-for (let i = 1; i <= 9; i++) {
-  const div = document.createElement("div");
-  div.className = "grid-item";
-  const img = document.createElement("img");
-  img.src = `https://picsum.photos/400/300?random=${i}`;
-  div.appendChild(img);
-  homeGallery.appendChild(div);
+function openImage(img){
+  document.getElementById("imageModal").style.display="block";
+  document.getElementById("modalImg").src=img.src;
 }
 
-
+function closeImage(){
+  document.getElementById("imageModal").style.display="none";
+}
