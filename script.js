aa6131f77fb8e051169aa6dff8684d35069207a6
sbfcorgs <sbfcorgs@gmail.com>
2026-02-14 01:41:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Smooth Scroll
-document.querySelectorAll('a.nav-link').forEach(link => {
-link.addEventListener('click', function(e){
-if(this.hash !== ""){
-e.preventDefault();
-document.querySelector(this.hash)
-.scrollIntoView({behavior:"smooth"});
-}
-});
-});
+// future animations or smooth scroll
