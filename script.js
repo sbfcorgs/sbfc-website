6a5ba1992b52c98f1223d50ba00c500a1178271b
sbfcorgs <sbfcorgs@gmail.com>
2026-02-13 19:36:37 +0400

    Remove image modal functions and add console log

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function openImage(img){
-  document.getElementById("imageModal").style.display="block";
-  document.getElementById("modalImg").src=img.src;
-}
-
-function closeImage(){
-  document.getElementById("imageModal").style.display="none";
-}
+console.log("SBFC Website Loaded Successfully");
