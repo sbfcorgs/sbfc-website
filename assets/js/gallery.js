0bd95364327ab830a680d137446d948260990478
sbfcorgs <sbfcorgs@gmail.com>
2026-02-14 16:50:36 +0400

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,8 @@
+function openLightbox(img){
+  document.getElementById("lightbox").style.display="flex";
+  document.getElementById("lightbox-img").src=img.src;
+}
 
+function closeLightbox(){
+  document.getElementById("lightbox").style.display="none";
+}
