bbad31260037c25e23ef16ce2e4aeb0669918b98
sbfcorgs <sbfcorgs@gmail.com>
2026-02-16 15:29:04 +0400

    Delete assets directory

--- assets/js/contact.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit",function(e){
-  e.preventDefault();
-  document.getElementById("successMsg").style.display="block";
-});
--- assets/js/donate.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("donateForm").addEventListener("submit",function(e){
-  e.preventDefault();
-  document.getElementById("thankYou").style.display="block";
-});
